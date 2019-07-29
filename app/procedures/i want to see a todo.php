9ea37034bb3a27895755d23005167f36c8964ea1
Aron Wouters <lionar@mail.com>
2019-07-29 10:55:28 +0200

    Corrected code style

--- app/procedures/i want to see a todo.php
+++ app/procedures/i want to see a todo.php
@@ -4,7 +4,7 @@ use function compact as with;
 
 when('i want to see a todo', then(apply(a( 
     
-function($id, todo\manager $manager)
+function($id, Todo\Manager $manager)
 {
     if (! $manager->has($id))
         return [2001, []];
