33ee3cc8f761ba36312ca73980a4ee7565f156d0
Aron Wouters <lionar@mail.com>
2019-07-29 11:14:25 +0200

    Removed input facade

--- io/api/routes/POST @.php
+++ io/api/routes/POST @.php
@@ -5,6 +5,7 @@ use Psr\Http\Message\ServerRequestInterface;
 
 Route::post('/', function (ServerRequestInterface $request): ResponseInterface
 {
-    list($status, $payload) = App::make('i want to add a todo');
+    $todo = App::make(Todo::class, (array) $request->getParsedBody());
+    list($status, $payload) = App::make('i want to add a todo', ['todo' => $todo]);
     return App::call(Status::match($status), $payload);
 });
