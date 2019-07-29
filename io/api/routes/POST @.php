215bf71403132903a60e8f76d2ed1375ba742953
Aron Wouters <lionar@mail.com>
2019-07-29 11:52:45 +0200

    Corrected data for json requests

--- io/api/routes/POST @.php
+++ io/api/routes/POST @.php
@@ -5,7 +5,7 @@ use Psr\Http\Message\ServerRequestInterface;
 
 Route::post('/', function (ServerRequestInterface $request): ResponseInterface
 {
-    $todo = App::make(Todo::class, (array) $request->getParsedBody());
+    $todo = App::make(Todo::class, (array) json_decode($request->getBody()));
     list($status, $payload) = App::make('i want to add a todo', ['todo' => $todo]);
     return App::call(Status::match($status), $payload);
 });
